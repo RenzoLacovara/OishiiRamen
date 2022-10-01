92e272f706e06473b7672f258a0861ba52b350d1
Renzo Lacovara <rnlacovara@gmail.com>
2022-09-30 23:10:43 -0300

    darkmode

--- script/index.js
+++ script/index.js
@@ -15,8 +15,63 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("is-active");
 });
 
-window.addEventListener("load", function () {
-  var bannerNode = document.querySelector('[alt="www.000webhost.com"]')
-    .parentNode.parentNode;
-  bannerNode.parentNode.removeChild(bannerNode);
-});
+// window.addEventListener("load", function () {
+//   var bannerNode = document.querySelector('[alt="www.000webhost.com"]')
+//     .parentNode.parentNode;
+//   bannerNode.parentNode.removeChild(bannerNode);
+// });
+const darkMode = () => {
+  const body = document.querySelector("body");
+  const systemDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+  const checkbox = document.querySelector("#mode");
+  let theme = window.localStorage.getItem("theme");
+  let preference;
+
+  const findSystemPreference = () => {
+    if (systemDarkMode.matches) {
+      preference = "dark";
+    } else {
+      preference = "light";
+    }
+  };
+
+  const setUserPreference = (e) => {
+    console.log(e.target);
+
+    if (e.target.checked == true) {
+      preference = "dark";
+      window.localStorage.setItem("theme", "dark");
+    } else if (e.target.checked == false) {
+      preference = "light";
+      window.localStorage.setItem("theme", "light");
+    }
+    setTheme(preference);
+  };
+
+  const setTheme = (pref) => {
+    if (pref === "dark") {
+      checkbox.checked = true;
+      body.classList.add("oscuro");
+    } else {
+      body.classList.remove("oscuro");
+      checkbox.checked = false;
+    }
+  };
+
+  const loadInitialState = () => {
+    if (theme === "dark") {
+      checkbox.checked = true;
+      body.classList.add("oscuro");
+    } else {
+      body.classList.remove("oscuro");
+
+      checkbox.checked = false;
+    }
+  };
+
+  checkbox.addEventListener("click", setUserPreference);
+  window.addEventListener("DOMContentLoaded", findSystemPreference);
+  window.addEventListener("DOMContentLoaded", loadInitialState);
+};
+
+darkMode();
