89bdda37d03e85afb80aa0406b81ae550a64e469
Renzo Lacovara <rnlacovara@gmail.com>
2022-10-06 01:13:49 -0300

    final

--- script/index.js
+++ script/index.js
@@ -80,3 +80,10 @@ function cerrarimg() {
     modalzoom.className = "modal-content";
   }, 400);
 }
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
